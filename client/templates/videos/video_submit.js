1e7b3e3d0d46815813b9fcf82b8e319b158f8826
David Cathers <dcathers1@gmail.com>
2016-03-17 12:59:04 -0500

    video submit logic in progress

--- client/templates/videos/video_submit.js
+++ client/templates/videos/video_submit.js
@@ -1,7 +1,34 @@
-Template.postSubmit.events({
+Template.videoSubmit.events({
     'submit form': function (e) {
         e.preventDefault();
 
-        //do stuff
+        var title = $("#vidTitle").val();
+        var tags = $("#vidTags").val();
+        var ytId = getIdFromUrl($("#vidUrl").val());
+        if (ytId != -1) {
+            //do final stuff
+        }
     }
 });
+
+function getIdFromUrl(url){
+    var strId = url.split('v=')[1];
+    var ampersandPosition = strId.indexOf('&');
+    if(ampersandPosition != -1) {
+        strId = strId.substring(0, ampersandPosition);
+    }
+    $.ajax({
+        url: "https://gdata.youtube.com/feeds/api/videos/" + strId + "?v=2&alt=json",
+        dataType: "jsonp",
+        success: function(data) {
+            console.log("success");
+            return strId;
+        },
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+            // Handle errors here
+            alert('ERRORS: ' + textStatus);
+            return -1;
+        }
+    });
+}
