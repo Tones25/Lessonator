c846f8a1b88b5a1694e4520b4753a32a7744a684
David Cathers <dcathers1@gmail.com>
2016-03-19 14:10:37 -0500

    working on comment update and delete (more)

--- client/templates/videos/comment_view.js
+++ client/templates/videos/comment_view.js
@@ -7,5 +7,17 @@ Template.commentView.helpers({
         if (this.userId === Meteor.userId()) {
             return true;
         }
+    },
+    edit: false
+});
+
+Template.commentView.events({
+    'click #edit': function () {
+        //not working yet
+        Template.commentView.edit = true;
+        console.log("doot");
+    },
+    'click #delete': function () {
+        Comments.remove({_id: this._id});
     }
 });
