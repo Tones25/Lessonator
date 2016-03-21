385824f65cf0d355b7b45d3531f0b99b4aa12485
Tony <tony@Tonys-MacBook-Pro.local>
2016-03-21 10:40:19 -0500

    -suggested content logic in place and working -other homepage views are reactive based on inserts and updates

--- client/templates/includes/homePage.js
+++ client/templates/includes/homePage.js
@@ -1,1 +1,9 @@
-
+/*Template.homePage.onCreated(function() {
+	var self = this;
+	self.autorun(function() {
+		self.subscribe('videosByRating');
+		self.subscribe('videosByNewest');
+		self.subscribe('videosBySuggested', Meteor.user());
+	});
+});
+*/
