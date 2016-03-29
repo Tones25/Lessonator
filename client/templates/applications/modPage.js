7c6fca8096a2d52a2884efdff616c3f9a395eb94
Tony <tony@Tonys-MacBook-Pro.local>
2016-03-29 17:29:17 -0500

    -small modifications to the mod page reportitems -changed the modpage helper back to what i had. Showing a message indicating there is stuff to do seems be a good aide for a new mod.

--- client/templates/applications/modPage.js
+++ client/templates/applications/modPage.js
@@ -1,17 +1,10 @@
 Template.modPage.helpers({
-//<<<<<<< HEAD
-    noResult: function(){
-        if(Videos.find().count() > 0){
-            return false;
-        }else{
-            return true;
-//=======
-//    message: function(){
-//        if(Videos.find().count() > 0){
-//            return "Here's some content that needs fixing:";
-//        }else{
-//            return "No flagged content at the moment!";
-//>>>>>>> c26faa2daee944051b6e9f0b32677019db256dd9
+
+    message: function() {
+        if(Videos.find().count() > 0) {
+            return 'Here is some content that needs fixing:';
+        } else {
+            return 'No flagged content at the moment!';
         }
     }
 });
--- client/templates/applications/report_item.js
+++ client/templates/applications/report_item.js
@@ -1,9 +1,23 @@
 Template.reportItem.helpers({
-	getFlags: function() {
+	getNotLessonReports: function() {
 		let notLesson = this.flagType.notLesson;
+
+		return notLesson;
+	},
+	getBadTitleReports: function() {
 		let badTitle = this.flagType.badTitle;
+
+		return badTitle;
+	},
+	getBadTagsReports: function() {
 		let badTags = this.flagType.badTags;
 
-		return 'Not a lesson: ' + notLesson + '   Bad title: ' + badTitle + '   Bad tags: ' + badTags;
+		return badTags;
+	},
+});
+Template.reportItem.events({
+	'click #ignoreButton': function(event, template) {
+		console.log(template);
+		$(this).remove();
 	}
 });
